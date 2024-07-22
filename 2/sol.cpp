83142f2b37acab485b956475742faea75478b7d3
akhilex <95928941+akhilex@users.noreply.github.com>
2024-07-23 00:06:11 +0530

    Create sol.cpp

---
+++ 2/sol.cpp
@@ -0,0 +1,12 @@
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    if(n%2!=0 || (n>=6 && n<=20)){
+        cout<<"Weird";
+    }
+    else{
+        cout<<"Not Weird";
+    }
+    return 0;
+}
